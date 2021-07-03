df13878be165d8709c01aeab84abab90208961c6
AVADHANULALKSRU <83009017+SrujanaAvadhanula@users.noreply.github.com>
2021-07-03 06:47:57 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  int x;
+  int y;
+  int sum;
+
+  printf("Enter value for x: ");
+  scanf("%d", &x);
+
+
+  printf("Enter value for y: ");
+  scanf("%d", &y);
+
+  sum=x+y;
+
+  printf("x = %d\n", x);
+  printf("y = %d\n", y);
+  printf("x +y = %d\n", sum);
+
+  printf("%d + %d = %d\n", x,y,sum);
+  printf("%d - %d = %d\n", x,y,(x-y));
+  printf("%d * %d = %d\n", x,y,(x*y));
+
+  return 0;
+}
